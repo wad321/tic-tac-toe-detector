635ffa02b918b29f6ac6f69499eda45b0cd26e58
wad321y <wad321y@gmail.com>
2017-11-12 14:35:08 +0100

    Commit#4

--- VideoProcessing.py
+++ VideoProcessing.py
@@ -48,29 +48,48 @@ def open_and_process_image(filename):
     return process_image(image_to_process)
 
 
-def initialize_svm(kernel, gamma, cache):
-    images = []
-    features = []
+def initialize_svn(samples, features, kernel, gamma, cache):
     svc = svm.SVC(kernel=kernel, gamma=gamma, cache_size=cache)
-    for file in glob.glob("plearn/*.jpg"):
-        images.append(open_and_process_image(file))
-        features.append('yes')
-
-    for file in glob.glob("pnotlearn/*.jpg"):
-        images.append(open_and_process_image(file))
-        features.append('no')
-
-    svc.fit(images, features)
+    svc.fit(samples, features)
     return svc
+
+
+def predict(svn, prediction):
+    print(svn.predict(prediction))
+
+
+def load_samples_and_labels(samples, features):
+    images = []
+    labels = []
+    for sample, feature in samples, features:
+        images.append(open_and_process_image(sample))
+        labels.append(feature)
+
+    return images, labels
 
 
 if __name__ == '__main__':
 
     pred = []
-    machine = initialize_svm('linear', 2, 1000)
+
+    images = []
+    labels = []
+    for file in glob.glob("plearn/*.jpg"):
+        images.append(open_and_process_image(file))
+        labels.append('yes')
+
+    for file in glob.glob("pnotlearn/*.jpg"):
+        images.append(open_and_process_image(file))
+        labels.append('no')
+
+    machine = initialize_svn(images, labels, 'linear', 2, 1000)
 
     pred.append(open_and_process_image('1.jpg'))
 
     pred.append(open_and_process_image('n1.jpg'))
 
-    print(machine.predict(pred))
+    p1 = Process(target=predict, args=(machine, pred,))
+    p2 = Process(target=predict, args=(machine, pred,))
+
+    p1.start()
+    p2.start()
