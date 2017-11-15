44da5480d83dd673e6f4a880423e769d47a36cd4
wad321y <wad321y@gmail.com>
2017-11-15 20:54:29 +0100

    Commit#14

--- VideoProcessing.py
+++ VideoProcessing.py
@@ -15,7 +15,9 @@ def process_frame(frame, changetograyscale):
     frame = imutils.resize(frame, width=size[0], inter=cv2.INTER_LANCZOS4)
     if changetograyscale:
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    frame = cv2.Canny(frame, 50, 200)
+    #frame = cv2.Canny(frame, 80, 200)
+    #cv2.imshow('image', frame)
+    #cv2.waitKey(0)
     frame = frame.astype(np.float64)
     frame = preprocessing.StandardScaler().fit(frame).transform(frame)
     frame = frame.flatten()
@@ -28,7 +30,7 @@ def open_and_process_image(filename):
 
 
 def initialize_svn(samples, features, c, gamma):
-    svc = svm.SVC(kernel='rbf', C=c, gamma=gamma, cache_size=1000)
+    svc = svm.SVC(kernel='rbf', C=c, cache_size=1000)
     svc.fit(samples, features)
     return svc
 
@@ -75,7 +77,7 @@ def show_frame(frame):
 
 
 def child_match_template(resized, template, ratio, interpolation, output_array):
-    edged = cv2.Canny(resized, 50, 200)
+    edged = cv2.Canny(resized, 80, 200)
     result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
     (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
     output_array[interpolation * 4] = maxVal
@@ -116,11 +118,14 @@ def get_matched_coordinates(image, template, interpolations):
     return found
 
 
-def prepare_template_from_image(image):
+def prepare_template_from_image(image, width):
+    image = imutils.resize(image, width=width)
     if image.shape[0] != image.shape[1]:
         image = crop_middle_square(image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    template = cv2.Canny(gray, 50, 200)
+    template = cv2.Canny(gray, 100, 200)
+    #cv2.imshow('template', template)
+    #cv2.waitKey(0)
     return template
 
 
@@ -147,22 +152,37 @@ def get_nine_images(image, start, end):
     for y in range(3):
         for x in range(3):
             crop = image[coords[1][x]:coords[1][x+1], coords[0][y]:coords[0][y+1]].copy()
-            #cv2.imshow('image', crop)
-            #cv2.waitKey(0)
+            cv2.imshow('crop', crop)
+            cv2.waitKey(0)
             nine_images.append(process_frame(crop, False))
 
     return nine_images
 
 
+def match_two_templates(image, start, end, circle, cross):
+    nine_images = []
+    coords = np.zeros((2, 4))
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    for i in range(2):
+        coords[i] = [start[i], int((2 * start[i] + end[i]) / 3), int((start[i] + 2 * end[i]) / 3), end[i]]
+    coords = coords.astype(np.int)
+
+    for y in range(3):
+        for x in range(3):
+            crop = image[coords[1][x]:coords[1][x+1], coords[0][y]:coords[0][y+1]].copy()
+            cv2.imshow('crop', crop)
+            cv2.waitKey(0)
+            canny = cv2.Canny(crop, 100, 200)
+
+
 if __name__ == '__main__':
 
     pred = []
-    img = cv2.imread("template2.jpg", cv2.IMREAD_COLOR)
+    img = cv2.imread("template1v2.jpg", cv2.IMREAD_COLOR)
     img2 = img.flatten()
     testimage = cv2.imread("plearn/2.jpg", cv2.IMREAD_COLOR)
     testimage = imutils.resize(testimage, width=800)
-    img = imutils.resize(img, width=256)
-    template = prepare_template_from_image(img)
+    template = prepare_template_from_image(img, 256)
 
     found = get_matched_coordinates(testimage, template, 20)
     (maxLoc0, maxLoc1, r) = found
@@ -180,14 +200,30 @@ if __name__ == '__main__':
 
     c = 2**(-5)
     gamma = 2**(-15)
+    answer = [-1, 0, 0, 0, 1, 0, 0, 0, -1]
 
-    while gamma <= 0.002:
-        while c <= 2**15:
+    while gamma <= 2 ** 2:
+        while c <= 2 ** 3:
+            goodhits = 0
             machine = initialize_svn(images, labels, c, gamma)
-            predict(machine, nineobjs, gamma, c)
+            array = []
+            for obj in machine.predict(nineobjs):
+                array.append(obj)
+            for i in range(9):
+                if answer[i] == array[i]:
+                    goodhits += 1
+            print(goodhits)
+            if goodhits >= 5:
+                output = open('output.txt', mode='a', encoding='utf-8')
+                print('HAHAHAHHA!')
+                line = str(str(goodhits) + " : " + str(gamma) + " " + str(c) + " " + " ".join(str(x) for x in array))
+                output.write(line + "\n")
+                output.close()
+
+            #predict(machine, nineobjs, gamma, c)
             c = c * 2
         c = 2 ** (-5)
-        gamma = gamma * 2
+    gamma = gamma * 2
 
 
     #pred.append(open_and_process_image('D:\MojeProjekty\PyCharm\VideoReader\IMG_20171114_110854.jpg'))
